bcb8732fcd167adf7c136232de1206f5f156fb18
Chia Wei Jie <chiaweijie2001@gmail.com>
2021-05-26 14:23:33 +0800

    barchart

--- demo/src/main/java/com/example/demo/controllers/IssueController.java
+++ demo/src/main/java/com/example/demo/controllers/IssueController.java
@@ -18,12 +18,14 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import javax.transaction.Transactional;
 
