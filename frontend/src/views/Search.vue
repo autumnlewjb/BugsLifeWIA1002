df0fb725a465ffa321ec7cf2c82d885e54612aaa
JBLew <autumnlewjb@gmail.com>
2021-06-10 19:08:39 +0800

    simplify search code

--- frontend/src/views/Search.vue
+++ frontend/src/views/Search.vue
@@ -126,287 +126,17 @@ export default {
       filterStatus: [],
       filterColor: '',
       multipleFilterAndSort: true,
+      searchDetails: {
+        keyword: '',
+        sort: [],
+        filter: {
+          tag: [],
+          status: []
+        }
+      },
     };
   },
   watch: {
-    search(val) {
-      if (val == null) {
-        return;
-      }
-      if (this.isLoading) {
-        return;
-      }
-
-      const focus = val
-        .substring(0, Math.max(val.indexOf(":"), 0))
-        .trim()
-        .toLowerCase();
-      const query = val
-        .substring(Math.max(val.indexOf(":") + 1, 0))
-        .trim()
-        .toLowerCase();
-      console.log(focus, query);
-
-      this.isLoading = true;
-      var url;
-      if (focus === "project") {
-        url = `/api?scope=project&query=${query}&size=5&page=0`;
-        this.searchType = "project";
-      } else if (focus == "issue") {
-        url = `/api?scope=issue&query=${query}&size=5&page=0`;
-        this.searchType = "issue";
-      } else if (focus == "user") {
-        url = `/api?scope=user&query=${query}&size=5&page=0`;
-        this.searchType = "user";
-      } else {
-        url = `/api?query=${query}&size=5&page=0`;
-        this.searchType = "all";
-      }
-
-      if (this.sortData.length > 0 && (this.searchType == "project" || this.searchType == "issue")) {
-        this.sortData.forEach((element) => {
-          url += `&sort=${element.subject},${element.order}`;
-        });
-      }
-
-      if (this.searchType == "issue") {
-        if (this.filterTags.length > 0) {
-          this.filterTags.forEach((element) => {
-            url += `&filter=tag,${element}`
-          });
-        }
-  
-        if (this.filterStatus.length > 0) {
-          this.filterStatus.forEach((element) => {
-            url += `&filter=status,${element}`
-          });
-        }
-      }
-
-      fetch(url)
-        .then((res) => {
-          if (res.status == 200) {
-            return res.json();
-          } else {
-            return {
-              content: [],
-              totalPages: 0,
-            };
-          }
-        })
-        .then((data) => {
-          this.isLoading = false;
-          this.query = query;
-          this.page = 1;
-          const { content, totalPages } = data;
-          this.totalPages = totalPages;
-          this.items = content;
-        });
-    },
-    page(val) {
-      console.log(val);
-      var url;
-      if (this.searchType === "project") {
-        url = `/api?scope=project`;
-      } else if (this.searchType == "issue") {
-        url = `/api?scope=issue`;
-      } else if (this.searchType == "user") {
-        url = `/api?scope=user`;
-      } else {
-        url = `/api?`;
-      }
-
-      url = `${url}&query=${this.query}&size=5&page=${Math.max(val - 1, 0)}`;
-      if (this.sortData.length > 0 && (this.searchType == "project" || this.searchType == "issue")) {
-        this.sortData.forEach((element) => {
-          url += `&sort=${element.subject},${element.order}`;
-        });
-      }
-
-      if (this.searchType == "issue") {
-        if (this.filterTags.length > 0) {
-          this.filterTags.forEach((element) => {
-            url += `&filter=tag,${element}`
-          });
-        }
-  
-        if (this.filterStatus.length > 0) {
-          this.filterStatus.forEach((element) => {
-            url += `&filter=status,${element}`
-          });
-        }
-      }
-
-      fetch(url)
-        .then((res) => {
-          if (res.status == 200) {
-            return res.json();
-          } else {
-            return {
-              content: [],
-              totalPages: 0,
-            };
-          }
-        })
-        .then((data) => {
-          const { content, totalPages, number } = data;
-          console.log(content);
-          this.page = number ? number + 1 : 1;
-          this.totalPages = totalPages;
-          this.items = content;
-        });
-    },
-    sortData(val) {
-      if (this.searchType != 'issue' && this.searchType != 'project') return;
-      if (val.length > 0) {
-        this.sortColor = "amber darken-4";
-      } else if (this.sortActive) {
-        this.sortColor = "primary";
-      }else {
-        this.sortColor = "undefined";
-      }
-      var url;
-      if (this.searchType === "project") {
-        url = `/api?scope=project`;
-      } else if (this.searchType == "issue") {
-        url = `/api?scope=issue`;
-      }
-
-      url = `${url}&query=${this.query}&size=5&page=${Math.max(this.page - 1, 0)}`;
-      if (val.length > 0) {
-        val.forEach((element) => {
-          url += `&sort=${element.subject},${element.order}`;
-        });
-      }
-      if (this.searchType == "issue") {
-        if (this.filterTags.length > 0) {
-          this.filterTags.forEach((element) => {
-            url += `&filter=tag,${element}`
-          });
-        }
-  
-        if (this.filterStatus.length > 0) {
-          this.filterStatus.forEach((element) => {
-            url += `&filter=status,${element}`
-          });
-        }
-      }
-      fetch(url)
-        .then((res) => {
-          if (res.status == 200) {
-            return res.json();
-          } else {
-            return {
-              content: [],
-              totalPages: 0,
-            };
-          }
-        })
-        .then((data) => {
-          const { content, totalPages, number } = data;
-          console.log(content);
-          this.page = number + 1;
-          this.totalPages = totalPages;
-          this.items = content;
-        });
-    },
-    filterStatus(val) {
-      console.log(val);
-      if (this.searchType != 'issue') return;
-      if (val.length > 0 || this.filterTags.length > 0) {
-        this.filterColor = "amber darken-4";
-      } else if (this.filterActive) {
-        this.filterColor = "primary";
-      } else {
-        this.filterColor = "undefined"
-      }
-      var url = `/api?scope=issue`;
-
-      url = `${url}&query=${this.query}&size=5&page=${Math.max(this.page - 1, 0)}`;
-      if (this.sortData.length > 0) {
-        this.sortData.forEach((element) => {
-          url += `&sort=${element.subject},${element.order}`;
-        });
-      }
-
-      if (this.filterStatus.length > 0) {
-        this.filterTags.forEach((element) => {
-          url += `&filter=tag,${element}`
-        });
-      }
-
-      if (val.length > 0) {
-        val.forEach((element) => {
-          url += `&filter=status,${element}`
-        });
-      }
-
-      fetch(url)
-        .then((res) => {
-          if (res.status == 200) {
-            return res.json();
-          } else {
-            return {
-              content: [],
-              totalPages: 0,
-            };
-          }
-        })
-        .then((data) => {
-          const { content, totalPages, number } = data;
-          console.log(content);
-          this.page = number ? number + 1 : 1;
-          this.totalPages = totalPages;
-          this.items = content;
-        });
-    },
-    filterTags(val) {
-      console.log("filterTags changed");
-      if (this.searchType != 'issue') return;
-      if (val.length > 0 || this.filterStatus.length > 0) {
-        this.filterColor = "amber darken-4";
-      } else if (this.filterActive) {
-        this.filterColor = "primary";
-      } else {
-        this.filterColor = "undefined"
-      }
-      var url = `/api?scope=issue`;
-
-      url = `${url}&query=${this.query}&size=5&page=${Math.max(this.page - 1, 0)}`;
-      if (this.sortData.length > 0) {
-        this.sortData.forEach((element) => {
-          url += `&sort=${element.subject},${element.order}`;
-        });
-      }
-      if (val.length > 0) {
-        val.forEach((element) => {
-          url += `&filter=tag,${element}`
-        });
-      }
-      if (this.filterStatus.length > 0) {
-        this.filterStatus.forEach((element) => {
-          url += `&filter=status,${element}`
-        });
-      }
-      fetch(url)
-        .then((res) => {
-          if (res.status == 200) {
-            return res.json();
-          } else {
-            return {
-              content: [],
-              totalPages: 0,
-            };
-          }
-        })
-        .then((data) => {
-          const { content, totalPages, number } = data;
-          console.log(content);
-          this.page = number ? number + 1 : 1;
-          this.totalPages = totalPages;
-          this.items = content;
-        });
-    },
     searchType(val) {
       if (val == 'issue') {
         this.availableSort = [
@@ -439,6 +169,52 @@ export default {
         tags: []
       };
       this.filterActive = false;
+    },
+    search() {
+      this.page = 1;
+    },
+    getSearchObject(val) {
+      if (val == null) {
+        return;
+      }
+      if (this.loading) {
+        return;
+      }
+      this.loading = true;
+      var url = `/api?query=${val.query}&scope=${val.scope}&size=5&page=${val.page}`;
+      this.searchType = val.scope;
+
+      val.sort.forEach((item) => {
+        url += `&sort=${item}`;
+      });
+
+      val.filter.tag.forEach((item) => {
+        url += `&filter=${item}`;
+      });
+
+      val.filter.status.forEach((item) => {
+        url += `&filter=${item}`;
+      });
+
+      fetch(url)
+        .then((res) => {
+          if (res.status == 200) {
+            return res.json();
+          } else {
+            return {
+              content: [],
+              totalPages: 0,
+            };
+          }
+        })
+        .then((data) => {
+          const { content, totalPages, number } = data;
+          console.log(data);
+          this.page = number ? number + 1 : 1;
+          this.totalPages = totalPages;
+          this.items = content;
+          this.loading = false;
+        });
     }
   },
   computed: {
@@ -451,11 +227,11 @@ export default {
         } else if (item.issueId) {
           item.id = item.issueId;
           item.route = "Issue";
-          item.type = "issue"
+          item.type = "issue";
           item.name = item.title;
           item.description = item.descriptionText;
         } else if (item.user_id) {
-          item.type = "user"
+          item.type = "user";
           item.name = item.username;
           item.description = item.email;
         }
@@ -480,6 +256,52 @@ export default {
       } else {
         return "undefined"
       }
+    },
+    getSearchObject() {
+      if (this.search == null) {
+        return;
+      }
+
+      const obj = {
+        page: Math.max(this.page - 1, 0),
+        query: '',
+        scope: '',
+        sort: [],
+        filter: {
+          tag: [],
+          status: []
+        }
+      };
+
+      obj.scope = this.search
+        .substring(0, Math.max(this.search.indexOf(":"), 0))
+        .trim()
+        .toLowerCase();
+      obj.query = this.search
+        .substring(Math.max(this.search.indexOf(":") + 1, 0))
+        .trim()
+        .toLowerCase();
+      
+      if (this.sortData.length > 0 && (this.searchType == "project" || this.searchType == "issue")) {
+        this.sortData.forEach((element) => {
+          obj.sort.push(`${element.subject},${element.order}`);
+        });
+      }
+
+      if (this.searchType == "issue") {
+        if (this.filterTags.length > 0) {
+          this.filterTags.forEach((element) => {
+            obj.filter.tag.push(`tag,${element}`);
+          });
+        }
+  
+        if (this.filterStatus.length > 0) {
+          this.filterStatus.forEach((element) => {
+            obj.filter.status.push(`status,${element}`);
+          });
+        }
+      }
+      return obj;
     }
   },
   methods: {
