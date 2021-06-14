e5fb5a7db7aec9ae526451117fd3871172793389
JBLew <autumnlewjb@gmail.com>
2021-06-14 14:01:10 +0800

    fix navigation problem on session expired

--- frontend/src/fetch.js
+++ frontend/src/fetch.js
@@ -5,9 +5,8 @@ const {fetch: originalFetch} = window;
 window.fetch = async (...args) => {
     const response = await originalFetch(...args);
     if (response.clone().status == 401) {
-        
         store.commit('setSessionExpired', true);
-        store.dispatch('logout');
+        await store.dispatch('logout');
     }
 
     return response;
