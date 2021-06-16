00ea2c1815457b167106e15d666ab2accd085ce1
Yuenmin <u2005375@siswa.um.edu.my>
2021-06-17 00:06:08 +0800

    Fix cannot clear filter

--- frontend/src/components/FilterForm.vue
+++ frontend/src/components/FilterForm.vue
@@ -1,28 +1,28 @@
 <template>
-    <v-container>
-      <v-row>
-        <v-col>
-          <v-form>
-            <v-row>
-              <v-col sm="12" md="12">
-                <v-combobox solo label="Issue Tags" v-model="filterTags" :items="tagOptions" multiple></v-combobox>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col sm="12" md="12">
-                <v-combobox solo label="Issue Status" v-model="filterStatus" :items="filterOptions"></v-combobox>
-              </v-col>
-            </v-row>
-            <v-row class="d-flex justify-end">
-              <v-btn text @click="clearAll">Clear All</v-btn>
-            </v-row>
-          </v-form>
-        </v-col>
-      </v-row>
-      <v-row class="my-10">
-        <v-divider></v-divider>
-      </v-row>
-    </v-container>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-form>
+          <v-row>
+            <v-col sm="12" md="12">
+              <v-combobox solo label="Issue Tags" v-model="filterTags" :items="tagOptions" multiple></v-combobox>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col sm="12" md="12">
+              <v-combobox solo label="Issue Status" v-model="filterStatus" :items="filterOptions"></v-combobox>
+            </v-col>
+          </v-row>
+          <v-row class="d-flex justify-end">
+            <v-btn text @click="clearAll">Clear All</v-btn>
+          </v-row>
+        </v-form>
+      </v-col>
+    </v-row>
+    <v-row class="my-10">
+      <v-divider></v-divider>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -51,22 +51,24 @@ export default {
   watch: {
     filterTags(val) {
       const len = this.filterTagsArray.length;
-      for (var i=0; i<len; i++) {
+      for (var i = 0; i < len; i++) {
         this.filterTagsArray.pop();
       }
       val?.forEach(element => this.filterTagsArray.push(element));
     },
     filterStatus(val) {
       const len = this.filterStatusArray.length;
-      for (var i=0; i<len; i++) {
+      for (var i = 0; i < len; i++) {
         this.filterStatusArray.pop();
       }
-      this.filterStatusArray.push(val);
+      if (val != null) {
+        this.filterStatusArray.push(val);
+      } else {
+        this.filterStatusArray = [];
+      }
     }
   },
-  computed: {
-
-  },
+  computed: {},
   methods: {
     clearAll() {
       this.filterTags = [];
